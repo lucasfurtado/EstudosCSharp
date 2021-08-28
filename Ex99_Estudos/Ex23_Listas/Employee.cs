6b5ca17042527eb8245eb11bfa053376c62b626d
Lucas Furtado <lucasfurtadodev@gmail.com>
2021-08-28 19:06:53 -0300

    exercicio de lista

---
+++ Ex99_Estudos/Ex23_Listas/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Ex23_Listas
+{
+    class Employee
+    {
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public void IncreaseSalary(double percentage)
+        {
+            Salary += Salary * (percentage/100);
+        }
+
+        public override string ToString()
+        {
+            return string.Concat(Id,", ",Name,", ",Salary.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Ex99_Estudos/Ex23_Listas/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Ex23_Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> listEmployee = new List<Employee>();
+
+            Console.Write("How many employees will be registered? ");
+            int numberEmployee = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberEmployee; i++)
+            {
+                Console.WriteLine(string.Concat("Employee #", i+1));
+                int auxId = int.Parse(Console.ReadLine());
+                string auxName = Console.ReadLine();
+                double auxSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Employee employee = new Employee(auxId, auxName, auxSalary);
+                listEmployee.Add(employee);
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int id = int.Parse(Console.ReadLine());
+            Employee auxEmployee = listEmployee.Find(x => x.Id == id);
+            if(auxEmployee != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                auxEmployee.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("Employee not found");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+            foreach (Employee item in listEmployee)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
